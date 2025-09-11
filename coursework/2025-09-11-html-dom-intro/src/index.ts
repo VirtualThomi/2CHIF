7083e8083b61997c1f088018e1ffcdea681345d8
Mona Lisgit config --global user.name Mona <t.henzl@students.htl-leonding.ac.at>
2025-09-11 16:41:33 +0200

    added select

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,17 +1,24 @@
 const counterButton =document.getElementById(`counterButton`) as HTMLButtonElement
 const decreaseButton =document.getElementById(`decreaseButton`) as HTMLButtonElement
 const counter =document.getElementById(`counter`) as HTMLParagraphElement
+const increment = document.getElementById(`increment`) as HTMLInputElement
+const select = document.getElementById(`incrementValue`) as HTMLSelectElement
 
 let clickCount = 0
+
+select.addEventListener(`change`,()=>{
+    increment.value=select.value
+})
+
 counterButton.addEventListener(`click`,()=>{
-    clickCount++
+    clickCount+= parseInt(increment.value)
     counter.textContent =`${clickCount}`
         if(clickCount>0){
         counter.style.color="green"
     }
 })
 decreaseButton.addEventListener(`click`,()=>{
-    clickCount--        
+    clickCount-= parseInt(increment.value)       
     counter.textContent =`${clickCount}`
     if(clickCount<0){
         counter.style.color="red"
