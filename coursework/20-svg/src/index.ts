faea067527f6879d06e89f4bfeddce6775f985e9
Mona Lisgit config --global user.name Mona <t.henzl@students.htl-leonding.ac.at>
2025-09-25 15:14:17 +0200

    Implement SVG circle addition on button click

--- coursework/20-svg/src/index.ts
+++ coursework/20-svg/src/index.ts
@@ -1,3 +1,15 @@
-const helloWorldHeader = document.getElementById("helloWorldHeader") as HTMLHeadingElement;
+const svg = document.getElementById("svg") as unknown as SVGSVGElement;
+const add = document.getElementById("add") as unknown as HTMLButtonElement;
 
-helloWorldHeader.innerText += "!";
+
+add.addEventListener("click", () =>{
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')as SVGCircleElement;
+
+    circle.setAttribute("fill","lime")
+    circle.setAttribute("stroke","green")
+    circle.setAttribute("cx", `${Math.random() * 200}`)
+     circle.setAttribute("cy", `${Math.random() * 200}`)
+    circle.setAttribute("r", `${Math.random() * 50}`)
+
+    svg.appendChild(circle)
+})
